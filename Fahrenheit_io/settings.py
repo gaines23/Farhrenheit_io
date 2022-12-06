d6412544c87b9f5adca8d28fa0edc26a097ae9f8
gaines23 <gaines.melissa23@gmail.com>
2022-12-06 02:47:37 -0800

    new updates

--- Fahrenheit_io/settings.py
+++ Fahrenheit_io/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'EcstaStream_Final',
+    'Fahrenheit_io',
     'multiselectfield',
     'social_django',
     'corsheaders',
@@ -59,10 +59,10 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.comware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messagmon.CommonMiddlees.middleware.MessageMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
